10543e13f6eec6247f37225ff6dcd09480fb229e
xwtttt <xwt9888@gmail.com>
2021-12-27 15:28:09 +0800

    改名

--- calc.py
+++
@@ -1,9 +0,0 @@
-from math import pow
-def calc_ADXL345(Range,Resolution, AD):
-    return ((2*Range)/(pow(2, Resolution)))*AD
-
-
-
-print(calc_ADXL345(8, 14, -34))
-print(calc_ADXL345(16, 13,  -10))
-# print(calc_MMA8451Q(76))
